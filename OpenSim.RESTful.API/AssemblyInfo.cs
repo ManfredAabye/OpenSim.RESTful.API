03dd1b37ab4cdb21596f5ea373b6c6784aeed941
ManfredAabye <zainhofer@gmx.de>
2024-12-26 14:54:39 +0100

    Add files via upload

---
+++ OpenSim.RESTful.API/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden durch die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("OpenSim.RESTful.API")]
+[assembly: AssemblyDescription("RESTful API for OpenSimulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Manni")]
+[assembly: AssemblyProduct("OpenSim.RESTful.API")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf TRUE fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("d0aadc44-12a7-4f8d-9aa0-1db7f3d8f07f")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die Standardwerte für die Buildnummer und Revision übernehmen, 
+// indem Sie "*" wie unten gezeigt verwenden:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
